28d83a092b726c76acc1364296f41305a34e530f
EC2 Default User <ec2-user@ip-172-31-4-98.us-west-2.compute.internal>
2013-09-07 18:43:27 +0000

    Updated models

--- PPPPP/models.py
+++ PPPPP/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 
 class Prize(models.Model):
-  name = models.CharField(max_length=200)
-  value = models.IntegerField(default=0)
   sponsor = models.ForeignKey('Sponsor')
+  title = models.CharField(max_length=200)
+  value = models.IntegerField(default=0)
+  def __unicode__(self):
+    return self.title
 
 class Technology(models.Model):
-  name = models.CharField(max_length=200)
-  url = models.URLField(max_length=200)
   sponsor = models.ForeignKey('Sponsor')
+  url = models.URLField(max_length=200)
+  def __unicode__(self):
+    return self.url
 
 class Sponsor(models.Model):
   name = models.CharField(max_length=200)
+  def __unicode__(self):
+    return self.name
